29fbb60702ad286d14d66059f275d55ff8f4c517
John Moreno <johnmorenoing@gmail.com>
2019-06-14 14:36:50 -0500

    Ajustes despliegue war

--- 2tf-rest-info/src/main/java/co/dostf/Application.java
+++ 2tf-rest-info/src/main/java/co/dostf/Application.java
@@ -2,11 +2,12 @@ package co.dostf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"co.dostf"})
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
 	public static void main(String ... args) {
 		SpringApplication.run(Application.class,args);
